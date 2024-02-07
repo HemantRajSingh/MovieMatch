b90c2c478731c20dbda1ca9d4ccfab0de001edf3
binayachaudari <binayachaudari@live.com>
2024-02-07 14:37:24 -0500

    omdb scraping sample added

---
+++ notebooks/scraping/omdb_scraping.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import requests
+import re
+
+df = pd.read_csv('movie_list_2000.csv')
+
+keys = ['Rated',
+        'Released',
+        'Runtime',
+        'Genre',
+        'Director',
+        'Writer',
+        'Actors',
+        'Plot',
+        'Language',
+        'Country',
+        'Awards',
+        'Poster',
+        'Ratings',
+        'imdbRating',
+        'BoxOffice',
+        'imdbID'
+      ]
+
+def get_movie_details(title):
+  r = requests.get(f'https://www.omdbapi.com/?apikey={key}&t={title}')
+  return r.json()
+
+for idx, row in df.iterrows():
+  movie_title = row['Movie Title']
+  title = re.sub(r'\s\(.+\)', '', movie_title)
+  year = row['Year']
+  data = get_movie_details(title)
+  print(data['Response'])
+  if data['Response'] == 'True':
+    for key in keys:
+      df.at[idx, key] = data.get(key, None)
+      df.at[idx, 'omdb Year'] = data.get('Year', None)
+      df.at[idx, 'omdb Title'] = data.get('Title', None)
+
+df.to_csv('movie_list_2000_omdb.csv')
+
