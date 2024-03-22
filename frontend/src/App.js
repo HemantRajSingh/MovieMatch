0b8d5faffc6dfee2cb2338a6d36d8ca196e73691
binayachaudari <binayachaudari@live.com>
2024-03-22 19:43:08 -0400

    UI for Movie match started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Header from './components/Header';
 
 const MovieRecommendations = () => {
   const [query, setQuery] = useState('');
@@ -12,7 +13,9 @@ const MovieRecommendations = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.get(`http://127.0.0.1:5000//recommendations/${query}`);
+      const response = await axios.get(
+        `http://127.0.0.1:5000//recommendations/${query}`
+      );
       setRecommendations(response.data);
     } catch (error) {
       console.error('Error fetching recommendations:', error);
@@ -21,25 +24,21 @@ const MovieRecommendations = () => {
 
   return (
     <div>
-      <h2>Get Movie Recommendations</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={query} onChange={handleChange} />
-        <button type="submit">Get Recommendations</button>
-      </form>
-      <ul>
-      {Array.isArray(recommendations) && recommendations.length > 0 ? (
-    recommendations.map((movie) => (
-        <li key={movie.id}>
-            <h3>{movie['Movie Title']}</h3>
-            <img src={movie['Cover Image']} alt={movie['Movie Title']} />
-            <p>Genre: {movie.Genre}</p>
-            <p>Year: {movie.Year}</p>
-        </li>
-    ))
-) : (
-    <li>No recommendations found</li>
-)}
-      </ul>
+      <Header />
+      {/* <ul>
+        {Array.isArray(recommendations) && recommendations.length > 0 ? (
+          recommendations.map((movie) => (
+            <li key={movie.id}>
+              <h3>{movie['Movie Title']}</h3>
+              <img src={movie['Cover Image']} alt={movie['Movie Title']} />
+              <p>Genre: {movie.Genre}</p>
+              <p>Year: {movie.Year}</p>
+            </li>
+          ))
+        ) : (
+          <li>No recommendations found</li>
+        )}
+      </ul> */}
     </div>
   );
 };
