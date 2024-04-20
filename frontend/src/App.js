cd733c879f07145ba174a920da74f9215ecd8756
binayachaudari <binayachaudari@live.com>
2024-04-20 02:09:53 -0400

    refactored main `app`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import Header from './components/Header';
 
 const dummyMovies = [
@@ -37,41 +36,10 @@ const dummyMovies = [
 
 const MovieRecommendations = () => {
   const [query, setQuery] = useState('');
-  const [recommendations, setRecommendations] = useState([]);
-
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.get(
-        `http://127.0.0.1:5000//recommendations/${query}`
-      );
-      setRecommendations(response.data);
-    } catch (error) {
-      console.error('Error fetching recommendations:', error);
-    }
-  };
 
   return (
     <div>
       <Header />
-      {/* <ul>
-        {Array.isArray(recommendations) && recommendations.length > 0 ? (
-          recommendations.map((movie) => (
-            <li key={movie.id}>
-              <h3>{movie['Movie Title']}</h3>
-              <img src={movie['Cover Image']} alt={movie['Movie Title']} />
-              <p>Genre: {movie.Genre}</p>
-              <p>Year: {movie.Year}</p>
-            </li>
-          ))
-        ) : (
-          <li>No recommendations found</li>
-        )}
-      </ul> */}
       <div className="flex w-3/4 mx-auto mt-3">
         <div className="items-center justify-between w-full mx-auto">
           <div className="space-y-1">
