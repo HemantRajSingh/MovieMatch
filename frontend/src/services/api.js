885537718a19498f06ecbb2680d0b1fd201d2f46
binayachaudari <binayachaudari@live.com>
2024-04-20 02:08:25 -0400

    api service refactored

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://127.0.0.1:5000',
 });
 
-export const recommendtaions = async ({ request }) => {
+export const getRecommendations = async ({ request }) => {
   try {
-    const response = await API.post(`/recommendations`, {
+    const response = await API.post(`/recommendations/`, {
       query: request,
     });
     return response.data;
