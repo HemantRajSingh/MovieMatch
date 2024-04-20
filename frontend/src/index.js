d15e0647e8ac1c8b565af1d938bdf64870be7e03
binayachaudari <binayachaudari@live.com>
2024-04-20 02:08:43 -0400

    added query client and context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MovieProvider } from './context/movieContext';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
