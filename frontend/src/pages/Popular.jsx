f12278113fc1f08199608e8c85193462b6cd1d40
binayachaudari <binayachaudari@live.com>
2024-04-20 09:30:36 -0400

    API implementation on Popular

--- frontend/src/pages/Popular.jsx
+++ frontend/src/pages/Popular.jsx
@@ -1,13 +1,20 @@
 import MovieList from '../components/MovieList';
 import Header from '../components/Header';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { getPopularMovies } from '../services/api';
+
 const Popular = () => {
+  const { data } = useQuery('latest', getPopularMovies);
   return (
     <>
       <Header />
       <div className="flex w-3/4 mx-auto mt-3">
-        (
-        <MovieList title="Popular" subtitle="Popular Movies" movieList={[]} />)
+        <MovieList
+          title="Popular"
+          subtitle="Popular Movies"
+          movieList={data ? data : []}
+        />
       </div>
     </>
   );
