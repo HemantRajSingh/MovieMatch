f1de5ebc2e659c62c0ea57b37f39e67a5fa01df8
binayachaudari <binayachaudari@live.com>
2024-04-20 09:30:49 -0400

    API implemenation on New

--- frontend/src/pages/New.jsx
+++ frontend/src/pages/New.jsx
@@ -1,14 +1,20 @@
 import MovieList from '../components/MovieList';
 import Header from '../components/Header';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { getLatestMovies } from '../services/api';
 
 const New = () => {
+  const { data } = useQuery('latest', getLatestMovies);
   return (
     <>
       <Header />
       <div className="flex w-3/4 mx-auto mt-3">
-        (
-        <MovieList title="New" subtitle="New Movies" movieList={[]} />)
+        <MovieList
+          title="New"
+          subtitle="New Movies"
+          movieList={data ? data : []}
+        />
       </div>
     </>
   );
