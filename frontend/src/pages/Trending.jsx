6e838473985a682bdcaaec666447fbd43309f11a
binayachaudari <binayachaudari@live.com>
2024-04-20 09:30:15 -0400

    API implementation on Trending

--- frontend/src/pages/Trending.jsx
+++ frontend/src/pages/Trending.jsx
@@ -1,15 +1,20 @@
 import MovieList from '../components/MovieList';
 import Header from '../components/Header';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { getTrending } from '../services/api';
 
 const Trending = () => {
+  const { data } = useQuery('trending', getTrending);
   return (
     <>
       <Header />
       <div className="flex w-3/4 mx-auto mt-3">
-        (
-        <MovieList title="Trending" subtitle="Trending Movies" movieList={[]} />
-        )
+        <MovieList
+          title="Trending"
+          subtitle="Trending Movies"
+          movieList={data ? data : []}
+        />
       </div>
     </>
   );
