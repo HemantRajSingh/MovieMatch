4cd8c49579eb20c9dd2b4dce7da97515d67845a3
binayachaudari <binayachaudari@live.com>
2024-04-20 09:30:04 -0400

    APIs implementation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,7 +12,51 @@ export const getRecommendations = async ({ query }) => {
     const response = await API.post(`/recommendations/`, {
       query: query,
     });
-    console.log(typeof response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+  }
+};
+
+export const getTrending = async () => {
+  try {
+    const response = await API.get(`/trending_movies/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+  }
+};
+
+export const getPopularMovies = async () => {
+  try {
+    const response = await API.get(`/popular_movies/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+  }
+};
+
+export const getLatestMovies = async () => {
+  try {
+    const response = await API.get(`/latest_movies/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+  }
+};
+
+export const getTopRatedMovies = async () => {
+  try {
+    const response = await API.get(`/top_rated_movies/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+  }
+};
+
+export const browseMovies = async () => {
+  try {
+    const response = await API.get(`/browse/`);
     return response.data;
   } catch (error) {
     console.error('Error fetching recommendations:', error);
