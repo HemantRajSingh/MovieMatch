d8b82f60202b6a66888677901e1c46c71e22badb
binayachaudari <binayachaudari@live.com>
2024-04-20 09:30:26 -0400

    API implementation on TopRated

--- frontend/src/pages/TopRated.jsx
+++ frontend/src/pages/TopRated.jsx
@@ -1,19 +1,20 @@
 import MovieList from '../components/MovieList';
 import Header from '../components/Header';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { getTopRatedMovies } from '../services/api';
 
 const TopRated = () => {
+  const { data } = useQuery('trending', getTopRatedMovies);
   return (
     <>
       <Header />
       <div className="flex w-3/4 mx-auto mt-3">
-        (
         <MovieList
           title="Top Rated"
           subtitle="Top Rated Movies"
-          movieList={[]}
+          movieList={data ? data : []}
         />
-        )
       </div>
     </>
   );
